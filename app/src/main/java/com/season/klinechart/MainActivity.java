5ab2f0db633a7efaf1bb85a3b4af168e0d5b41ad
451360508@qq.com <451360508@qq.com>
2021-07-27 15:15:30 +0800

    Initial commit

---
+++ app/src/main/java/com/season/klinechart/MainActivity.java
@@ -0,0 +1,14 @@
+package com.season.klinechart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
