a65c18fa82b28823d122668a3a15f16f6a63ab88
451360508@qq.com <451360508@qq.com>
2021-08-05 16:42:18 +0800

    界面细节调整

--- app/src/main/java/com/season/klinechart/MainActivity.java
+++ app/src/main/java/com/season/klinechart/MainActivity.java
@@ -1,20 +1,17 @@
 package com.season.klinechart;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.fujianlian.klinechart.DataHelper;
 import com.github.fujianlian.klinechart.KLineChartAdapter;
@@ -27,7 +24,7 @@ import com.google.android.material.tabs.TabLayout;
 import java.text.DecimalFormat;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
 
     KLineChartView kLineChartView;
@@ -36,34 +33,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chart);
 
-        initView();
         kLineChartView = findViewById(R.id.kLineChartView);
         adapter = new KLineChartAdapter();
         kLineChartView.setDateTimeFormatter(new DateFormatter());
         kLineChartView.setOverScrollRange(138);
-        kLineChartView.setGridRows(4);
-        kLineChartView.setGridColumns(4);
+        kLineChartView.setGridRows(5);
+        kLineChartView.setGridColumns(5);
         kLineChartView.setAdapter(adapter);
 
+        initView();
         kLineChartView.justShowLoading();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                List<KLineEntity> data = DataRequest.getALL(MainActivity.this);
-                DataHelper.calculate(data);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        adapter.addFooterData(data);
-                        adapter.notifyDataSetChanged();
-                        //kLineChartView.scrollBy(-200, 0);
-                        kLineChartView.startAnimation();
-                        kLineChartView.refreshEnd();
-                    }
-                });
-            }
+        new Thread(() -> {
+            List<KLineEntity> data = DataRequest.getALL(MainActivity.this);
+            DataHelper.calculate(data);
+            runOnUiThread(() -> {
+                adapter.addFooterData(data);
+                adapter.notifyDataSetChanged();
+                kLineChartView.startAnimation();
+                kLineChartView.refreshEnd();
+            });
         }).start();
 
 
@@ -79,13 +69,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private TextView tv_coin_title;
     private TextView k_Price;
-    private TextView K_line;
     private TextView approx;
     private TextView apprise;
     private TextView app_zuigao;
     private TextView app_zuidi;
     private TextView app_liang;
-    private LinearLayout amountL;
     private TextView text_zuigao;
     private TextView text_zuidi;
     private TextView text_liang;
@@ -102,7 +90,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private PopupWindow popupWindow;
     private TextView maText;
     private TextView bollText;
-    private TextView mainHide;
     private RadioGroup k_Group;
 
     private PopupWindow timePopup;
@@ -112,21 +99,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView textViewFar;
     private void initView() {
         k_Price = findViewById(R.id.k_Price);
-        K_line = findViewById(R.id.K_line);
         approx = findViewById(R.id.approx);
         apprise = findViewById(R.id.apprise);
         app_zuigao = findViewById(R.id.app_zuigao);
         app_zuidi = findViewById(R.id.app_zuidi);
         app_liang = findViewById(R.id.app_liang);
-        amountL = findViewById(R.id.amountL);
         text_zuigao = findViewById(R.id.text_zuigao);
         text_zuidi = findViewById(R.id.text_zuidi);
         text_liang = findViewById(R.id.text_liang);
-        tv_text_fen0 = findViewById(R.id.tv_text_fen0);
-        tv_text_fen1 = findViewById(R.id.tv_text_fen1);
-        tv_text_fen5 = findViewById(R.id.tv_text_fen5);
-        tv_text_fen15 = findViewById(R.id.tv_text_fen15);
-        tv_text_fen30 = findViewById(R.id.tv_text_fen30);
+
+
         time_all = findViewById(R.id.time_all);
         k_index = findViewById(R.id.k_index);
         tab_layout = findViewById(R.id.tab_layout);
@@ -139,54 +121,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             finish();
         });
 
-        tv_text_fen0.setOnClickListener(o -> // 分时
-        {
-            timeSwitch("0", tv_text_fen0);
-        });
-        tv_text_fen1.setOnClickListener(o -> // 分时K线：点击1分钟
-        {
-            timeSwitch("1", tv_text_fen1);
-        });
-        tv_text_fen5.setOnClickListener(o -> // 分时K线：点击5分钟
-        {
-            timeSwitch("5", tv_text_fen5);
-        });
-        tv_text_fen15.setOnClickListener(o -> // 分时K线：点击15分钟
-        {
-            timeSwitch("15", tv_text_fen15);
-        });
-        tv_text_fen30.setOnClickListener(o -> // 分时K线：点击30分钟
-        {
-            timeSwitch("30", tv_text_fen30);
-        });
+        for (int i = 0; i < textIds.length; i++) {
+            TextView textView = findViewById(textIds[i]);
+            int finalI = i;
+            textView.setOnClickListener(v -> {
+                timeSwitch(intervals[finalI], textView);
+            });
+        }
         time_all.setOnClickListener(o -> // 分时K线：点击更多
         {
-            timePopup.showAsDropDown(time_all);
+            timePopup.showAsDropDown(time_all, 0, 38);
         });
         k_index.setOnClickListener(o -> // 分时K线：点击指标
         {
-            popupWindow.showAsDropDown(k_index);
+            popupWindow.showAsDropDown(k_index, 0, 38);
         });
     }
 
 
+    private void resetMaBoll(int index){
+        maText.setTextColor(getResources().getColor(index == 0?R.color.chart_line:R.color.chart_text));
+        maText.setBackgroundResource(index == 0?R.drawable.btn_line_deep:R.drawable.btn_circle_deep);
+
+        bollText.setTextColor(getResources().getColor(index == 1?R.color.chart_line:R.color.chart_text));
+        bollText.setBackgroundResource(index == 1?R.drawable.btn_line_deep:R.drawable.btn_circle_deep);
+
+        if (index == 0) {
+            kLineChartView.changeMainDrawType(Status.MA);
+        }else if (index == 1) {
+            kLineChartView.changeMainDrawType(Status.BOLL);
+        }else{
+            kLineChartView.changeMainDrawType(Status.NONE);
+        }
+    }
     private void initIndexPopup() {
         final View contentView = LayoutInflater.from(MainActivity.this).inflate(R.layout.kc_index_popup, null);
 
         maText = contentView.findViewById(R.id.maText);
         bollText = contentView.findViewById(R.id.bollText);
-        mainHide = contentView.findViewById(R.id.mainHide);
-        maText.setOnClickListener(this);
-        bollText.setOnClickListener(this);
-        mainHide.setOnClickListener(this);
+        maText.setOnClickListener(v -> {
+            kLineChartView.hideSelectData();
+            //主图 MA线
+            if (mainIndex != 0) {
+                mainIndex = 0;
+            }else{
+                mainIndex = -1;
+            }
+            resetMaBoll(mainIndex);
+        });
+        bollText.setOnClickListener(v -> {
+            kLineChartView.hideSelectData();
+            //主图 BOLL线
+            if (mainIndex != 1) {
+                mainIndex = 1;
+            }else{
+                mainIndex = -1;
+            }
+            resetMaBoll(mainIndex);
+        });
+        mainIndex = 0;
+        resetMaBoll(mainIndex);
 
         k_Group = contentView.findViewById(R.id.k_Group);
 
         popupWindow = new PopupWindow(contentView,
-                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
         popupWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
         popupWindow.setOutsideTouchable(true);
         popupWindow.setTouchable(true);
+        popupWindow.setAnimationStyle(R.style.contextMenuAnim);
         k_Group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -204,79 +207,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } else if (k_Group.getCheckedRadioButtonId() == R.id.wrText) {
                     subIndex = 3;
                     kLineChartView.setChildDraw(subIndex);
-                } else if (k_Group.getCheckedRadioButtonId() == R.id.subHide) {
-                    subIndex = -1;
-                    kLineChartView.hideChildDraw();
                 }
                 radioButton.setChecked(true);
             }
         });
+        k_Group.check(R.id.macdText);
     }
 
+    View timeView;
     private void initTimePopView() {
-        final View timeView = LayoutInflater.from(MainActivity.this).inflate(R.layout.kc_time_popup, null);
-        timePopup = new PopupWindow(timeView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
-        timePopup.setBackgroundDrawable(new ColorDrawable(0x00000000));
+        timeView = LayoutInflater.from(MainActivity.this).inflate(R.layout.kc_time_popup, null);
+        timePopup = new PopupWindow(timeView, ViewGroup.LayoutParams.MATCH_PARENT,
+                (int) (getResources().getDisplayMetrics().density * 48 ));
+        timePopup.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         timePopup.setOutsideTouchable(true);
-        timePopup.setTouchable(true);
+        timePopup.setAnimationStyle(R.style.contextMenuAnim);
+        timePopup.setFocusable(true);
 
-        textViewSve = timeView.findViewById(R.id.textViewSve);
-        textViewNine = timeView.findViewById(R.id.textViewNine);
-        textViewSex = timeView.findViewById(R.id.textViewSex);
-        textViewFar = timeView.findViewById(R.id.textViewFar);
-        textViewSve.setOnClickListener(this);
-        textViewNine.setOnClickListener(this);
-        textViewSex.setOnClickListener(this);
-        textViewFar.setOnClickListener(this);
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        //60, 240, 1D, 1W
-        int id = v.getId();
-        if (id == R.id.textViewSve) {
-            //60分钟
-            timeSwitch("60", textViewSve);
-        } else if (id == R.id.textViewNine) {
-            //4小时
-            timeSwitch("240", textViewNine);
-        } else if (id == R.id.textViewSex) {
-            //日线
-            timeSwitch("1D", textViewSex);
-        } else if (id == R.id.textViewFar) {
-            //周线
-            timeSwitch("1W", textViewFar);
-        } else if (id == R.id.maText) {
-            //主图 MA线
-            if (mainIndex != 0) {
-                kLineChartView.hideSelectData();
-                mainIndex = 0;
-                maText.setTextColor(Color.parseColor("#eeb350"));
-                bollText.setTextColor(Color.WHITE);
-                kLineChartView.changeMainDrawType(Status.MA);
-            }
-        } else if (id == R.id.bollText) {
-            //主图 BOLL线
-            if (mainIndex != 1) {
-                kLineChartView.hideSelectData();
-                mainIndex = 1;
-                bollText.setTextColor(Color.parseColor("#eeb350"));
-                maText.setTextColor(Color.WHITE);
-                kLineChartView.changeMainDrawType(Status.BOLL);
-            }
-        } else if (id == R.id.mainHide) {
-            //主图 隐藏
-            if (mainIndex != -1) {
-                kLineChartView.hideSelectData();
-                mainIndex = -1;
-                bollText.setTextColor(Color.WHITE);
-                maText.setTextColor(Color.WHITE);
-                mainHide.setTextColor(Color.parseColor("#1e82d2"));
-                kLineChartView.changeMainDrawType(Status.NONE);
-            }
+        for (int i = 0; i < textIdsPop.length; i++) {
+            TextView textView = timeView.findViewById(textIdsPop[i]);
+            int finalI = i;
+            textView.setOnClickListener(v -> {
+                timeSwitch(intervalsPop[finalI], textView);
+            });
         }
     }
+
 
     private String currentTimeSwitch = "-1";//当前显示的分时类型   默认1分钟
     private boolean isReceivedSub = false;//是否继续接收Sub数据
@@ -290,42 +246,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //CmdUtil.cmdReq(mCoinCode, currentTimeSwitch);//查询历史
         }
 
-        switch (interval) {
-            case "0"://分时
-                resetFenShiColor();
-                selectTextView.setTextColor(Color.parseColor("#1e82d2"));
-                kLineChartView.hideSelectData();
-                kLineChartView.setMainDrawLine(true);
-                break;
-            case "1"://1分钟
-            case "1D"://1天
-            case "1W"://7天
-            case "5"://5分钟
-            case "15"://15分钟
-            case "30"://30分钟
-            case "60"://60分钟
-            case "240"://4小时
-                resetFenShiColor();
-                selectTextView.setTextColor(Color.parseColor("#1e82d2"));
-                kLineChartView.hideSelectData();
-                kLineChartView.setMainDrawLine(false);
-                break;
+        kLineChartView.hideSelectData();
+
+        for (int i = 0; i < intervals.length; i++) {
+            TextView textView = findViewById(textIds[i]);
+            if (interval.equals(intervals[i])){
+                textView.setTextColor(getResources().getColor(R.color.chart_line));
+            }else{
+                textView.setTextColor(getResources().getColor(R.color.chart_text));
+            }
+        }
+
+        for (int i = 0; i < intervalsPop.length; i++) {
+            TextView textView = timeView.findViewById(textIdsPop[i]);
+            if (interval.equals(intervalsPop[i])){
+                textView.setTextColor(getResources().getColor(R.color.chart_line));
+                textView.setBackgroundResource(R.drawable.btn_line_deep);
+            }else{
+                textView.setTextColor(getResources().getColor(R.color.chart_text));
+                textView.setBackgroundResource(R.drawable.btn_circle_deep);
+            }
+        }
+        if ("0".equals(interval)){
+            kLineChartView.setMainDrawLine(true);
+        }else{
+            kLineChartView.setMainDrawLine(false);
         }
     }
-    /**
-     * 重置副图文字颜色
-     */
-    private void resetFenShiColor() {
-        tv_text_fen0.setTextColor(this.getResources().getColor(R.color.F888888));
-        tv_text_fen1.setTextColor(this.getResources().getColor(R.color.F888888));
-        tv_text_fen5.setTextColor(this.getResources().getColor(R.color.F888888));
-        tv_text_fen15.setTextColor(this.getResources().getColor(R.color.F888888));
-        tv_text_fen30.setTextColor(this.getResources().getColor(R.color.F888888));
-        /******************/
-        textViewSve.setTextColor(this.getResources().getColor(R.color.F888888));
-        textViewNine.setTextColor(this.getResources().getColor(R.color.F888888));
-        textViewSex.setTextColor(this.getResources().getColor(R.color.F888888));
-        textViewFar.setTextColor(this.getResources().getColor(R.color.F888888));
-    }
+
+    final int[] textIdsPop = {R.id.tv_text_fen0, R.id.tv_text_fen1, R.id.tv_text_fen5, R.id.tv_text_fen30, R.id.tv_text_mouth};
+    final String[] intervalsPop = {"0", "1", "5", "30", "1M"};
+
+    final int[] textIds = {R.id.tv_text_fen15, R.id.tv_text_hour1, R.id.tv_text_hour4, R.id.tv_text_day, R.id.tv_text_week};
+    final String[] intervals = {"15", "60", "240", "1D", "1W"};
+
 
 }
